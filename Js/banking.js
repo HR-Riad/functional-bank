60c447a1528813d10a4fefe3af30eadd514f0e7b
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-14 12:16:25 +0600

    Update deposit and balance and handle string add

--- Js/banking.js
+++ Js/banking.js
@@ -1,29 +1,43 @@
-/* document.getElementById('deposite-button').addEventListener('click', function(){
-const depositInput = document.getElementById('deposit-input');
 
-const depositAmount = depositInput.value ;
-
-// get current deposit 
-
-const depositTotal = document.getElementById('deposit-total');
-
-const  depositTotalValue = depositTotal.innerText;
-
-depositTotal.innerText = depositAmount;
-
-}) */
 
 
 document.getElementById('deposite-button').addEventListener('click',function()
 {
 
     const depositInput = document.getElementById('deposit-input');
-    
-    const depositAmount = depositInput.value ;
+    const depositAmountValue =  depositInput.value ;
+    const depositAmount = parseFloat(depositAmountValue);
 
     const depositTotal = document.getElementById('deposit-total');
+    const depositTotalValue = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalValue);
 
-    const DepositeTotalValue = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
+
+     depositTotal.innerText = previousDepositTotal + depositAmount;
+    
+ 
+/* update balance */
+const balanceTotal =document.getElementById ('balance-total');
+const balanceAmountValue = balanceTotal.innerText;
+const previousBalanceTotal =  parseFloat(balanceAmountValue);
+
+balanceTotal.innerText = previousBalanceTotal + depositAmount;
+  
+
     depositInput.value=''; 
 })
+/* 
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountValue = withdrawInput.value ;
+    const withdrawAmount = parseFloat (withdrawAmountValue);
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalValue =withdrawTotal.innerText;
+    const previouswithdrawTotal = parseFloat(withdrawTotalValue);
+
+    withdrawTotal.innerText = previouswithdrawTotal + withdrawAmount;
+
+    withdrawInput.value = '';
+    
+}) */
